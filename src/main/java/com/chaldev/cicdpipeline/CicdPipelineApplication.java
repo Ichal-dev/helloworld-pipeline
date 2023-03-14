a74af9b4cecdabbe0bfe03ba20a060c698cf5bab
Ichal-dev <calprogrammer08@gmail.com>
2023-03-15 02:30:51 +0800

    Jenkinsfile

--- src/main/java/com/chaldev/cicdpipeline/CicdPipelineApplication.java
+++ src/main/java/com/chaldev/cicdpipeline/CicdPipelineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CicdpipelineApplication {
+public class CicdPipelineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CicdpipelineApplication.class, args);
+        SpringApplication.run(CicdPipelineApplication.class, args);
     }
 
 }
--- src/test/java/com/chaldev/cicdpipeline/CicdPipelineApplicationTests.java
+++ src/test/java/com/chaldev/cicdpipeline/CicdPipelineApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CicdpipelineApplicationTests {
+class CicdPipelineApplicationTests {
 
     @Test
     void contextLoads() {
